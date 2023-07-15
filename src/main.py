fe4ef9a4859106d017ad9f74fa24d05b254df056
sdregster <105959977+sdregster@users.noreply.github.com>
2023-07-15 14:41:10 +0300

    quick fix
    fixed the situation when opened excel files got into script process

--- src/main.py
+++ src/main.py
@@ -9,12 +9,13 @@ if __name__ == "__main__":
     input_folder_path = os.path.join(os.getcwd(), "!input")
     found_estimates = []
     for file in os.listdir(input_folder_path):
-        file_path = os.path.join(input_folder_path, file)
+        if '~' not in file:
+            file_path = os.path.join(input_folder_path, file)
 
-        temp_estimate = EstimateBaseTemplate(file_path)
-        temp_estimate.read_rows()
+            temp_estimate = EstimateBaseTemplate(file_path)
+            temp_estimate.read_rows()
 
-        found_estimates.append(temp_estimate)
+            found_estimates.append(temp_estimate)
 
     # Экспорт в шаблон неосинтеза
     NeosintezTemplate().export(found_estimates)
